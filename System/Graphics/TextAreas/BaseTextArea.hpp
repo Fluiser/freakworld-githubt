749036b645a84b035ce536a99c524c1a54bc8cfe
Fluiser <tyrkalov2.restart@gmail.com>
2023-04-02 14:22:38 +0300

    yep

---
+++ System/Graphics/TextAreas/BaseTextArea.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+namespace Scene {
+	class Scene;
+}
+
+namespace Engine {
+	class Window;
+	namespace Graphics {
+		namespace TextAreas {
+			class BaseTextArea
+			{
+			public:
+				BaseTextArea(sf::Font, sf::Vector2i);
+				~BaseTextArea();
+
+				void setCharacterSize(int);
+				void render(Engine::Window& window, const Scene::Scene& scene) const;
+				void clear();
+
+				void setPosition(float x, float y);
+
+				sf::Text& operator[](int i);
+
+				size_t getLength() const;
+			protected:
+				std::vector<sf::Text> parts;
+				sf::Vector2i offset = { 0,0 };
+				sf::Vector2i position = {};
+				int charSize = 72;
+				sf::Font _font;
+			};
+		}
+	}
+}
